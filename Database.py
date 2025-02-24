b092b7d3f1f14825435ab506ca559cf51953af02
Joseph Quiroz <joqu6657@stu.sjcc.edu>
2025-02-24 03:04:24 -0800

    Optimized database creation
    I realized there was a better way to create the database instead of initializing  30 dictionaries and storing them in a list
    
    It is instead stored in a nested dictionary which makes it way easier to access player data. All you need is the team and player name

--- Database.py
+++ Database.py
@@ -3,71 +3,59 @@ import os
 import glob
 import math
 
-numberOfDataFrames = 5
-streakCount = 0
-variables = ['PTS','TRB','AST','PRA','PA','PR','RA','3P']
-storedData = []
-
-ATL = {}
-CLE = {}
-BOS = {}
-OKC = {}
-MIN = {}
-
-listOfTeams = [OKC , MIN]
-
 folderPath = r"C:\Users\josep\OneDrive\Desktop\Personal\NBA-prop-tool\Players"
+variables = ['PTS','TRB','AST','PRA','PA','PR','RA','3P']
+dictonaryOfTeams={}
 
-# Assigns the player data to their respective team dictionary
-def findTeam(teamName, playerName, dataFrame = None):
-    match teamName:
-        case "ATL":
-            ATL[playerName] = dataFrame
-        case "BOS": 
-            BOS[playerName] = dataFrame
-        case "CLE":
-            CLE[playerName] = dataFrame
-        case "OKC":
-            OKC[playerName] = dataFrame
-        case "MIN":
-            MIN[playerName] = dataFrame
-        case " _":
-            print("Something went wrong")
-          
-# Reads the player excel file and create a dataframe of it            
-def createDataFrame(path, folderName):
-    for playerName in os.listdir(path): # For loop which goes through each file in folder
-        if playerName.endswith(('.xls')): # Checks if it is '.xls' file
-            filePath = os.path.join(path, playerName) # Creates path to xls file
-            playerName = playerName.replace(".xls","") # Creates the key for the dictionary, which is the player's name
-            try: # Creates dataframe keeping certain collumns
-             df = pd.read_excel(filePath, engine='xlrd', usecols=['G','Tm','Opp','3P','TRB','AST','PTS'])
-             df = df[df['G'] >= 0] # Only keeps data for games they played
-             df['PRA'] = df['PTS'] + df['TRB'] + df['AST']
-             df['PR'] =  df['PTS'] + df['TRB']
-             df['PA'] =  df['PTS'] + df['AST']
-             df['RA'] =  df['TRB'] + df['AST']
-             findTeam(folderName, playerName, df) # Assigns data to respective team dictionary
+# Creates database to retrieve data from
+def initilizeDatabase():
+    # For loop which goes through each team folder in "Player" folder
+    for teamName in os.listdir(folderPath):
+        # Creates path to team folder
+        filePath = os.path.join(folderPath, teamName)
+        # Creates a nested dictionary where the Key is the team name and the value
+        # is another dictonary of player data
+        dictonaryOfTeams[teamName] = createPlayerDictionary(filePath)
+        
+# Returns a dictionary where the key is the player name and the value is
+# a data frame of the player stats
+def createPlayerDictionary(filePath):
+    playerData = {}
+    # For loop which goes through each player excel file in the team folder
+    for playerName in os.listdir(filePath):
+         # Checks if it is '.xls' file
+        if playerName.endswith(('.xls')):
+            # Creates path to xls file
+            currentFilePath = os.path.join(filePath, playerName)
+            # Creates the key for the dictionary, which is the player's name
+            playerName = playerName.replace(".xls","") 
+            try:
+                # Creates dataframe keeping certain collumns
+                df = pd.read_excel(currentFilePath, engine='xlrd', usecols=['G','Tm','Opp','3P','TRB','AST','PTS'])
+                # Only keeps data for games they played
+                df = df[df['G'] >= 0]
+                # Adds addtional collumns of data
+                df['PRA'] = df['PTS'] + df['TRB'] + df['AST']
+                df['PR'] =  df['PTS'] + df['TRB']
+                df['PA'] =  df['PTS'] + df['AST']
+                df['RA'] =  df['TRB'] + df['AST']
+                playerData[playerName] = df
             except Exception as e:
                 print(f"Error reading {playerName}: {e}")
+    return playerData
 
-# Scans the "Players" Folder, which has subfolders of each team
-def folderScan(path):
-    for folderTeamName in os.listdir(path): #For loop which goes through each team folder in "Player" folder
-        filePath = os.path.join(path, folderTeamName) # Creates path to folder
-        createDataFrame(filePath, folderTeamName) # Runs funtion to create dataframes for each xls file in team folder
-
-
-def findStreak(playerData, playerName):
-    playerData = playerData[::-1]
+# Prints the hit rate of player props with a given line
+def findStreak(teamName, playerName):
+    # Reverses the data frame
+    playerData = dictonaryOfTeams[teamName][playerName][::-1]
+    # Itterates each collumn
     for i in variables:
         streakCount = 0
         strikeCount = 0
         totalGames = 0
-       # targetValue = math.ceil(playerData[i].mean() - playerData[i].std())
-        targetValue = math.floor(playerData[i].mean()*0.9)
+        targetValue = math.ceil(playerData[i].mean() * 0.8)
         for j in playerData.index:
-            if strikeCount > 1:
+            if strikeCount > 2:
                 print(playerName +" has a had " + str(targetValue) + " " + i + " in " + str(streakCount) + " of " + str(totalGames) + " games")
                 break
             elif(playerData.loc[j,i]< targetValue):
@@ -76,19 +64,11 @@ def findStreak(playerData, playerName):
             else:
                  streakCount += 1
                  totalGames += 1
-
-                             
-folderScan(folderPath)
-
-
-for team in listOfTeams:
-    for player in team.keys():
-        findStreak(team[player],player)
-
-
-#EX = CLE["Darrius Garland"]
-#print(MIN.keys())
-
-#findStreak(EX)
+        
+    
+initilizeDatabase()
+#findStreak("OKC","Jalen Williams")
+#print("---------------------")
+#findStreak("CLE", "Donovan Mitchell")
 
 
